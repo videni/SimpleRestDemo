e4c8a57ef1a047bc7d892152af141f7945b27b94
vidy <zhengquan.li@remarkdip.com>
2018-11-20 14:51:52 +0800

    implememnt create resource action

--- src/Bundle/AppBundle/Factory/CommentFactory.php
+++ src/Bundle/AppBundle/Factory/CommentFactory.php
@@ -2,19 +2,27 @@
 
 declare(strict_types=1);
 
-namespace App\Bundle\RestBundle\Factory;
+namespace App\Bundle\AppBundle\Factory;
 
+use App\Bundle\RestBundle\Factory\Factory;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use App\Bundle\AppBundle\Repository\PostRepository;
 
 class CommentFactory extends Factory
 {
     private $postRepository;
+    private $tokenStorage;
 
-    public function __construct($className, PostRepository $postRepository)
-    {
+    public function __construct(
+        $className,
+        PostRepository $postRepository,
+        TokenStorageInterface $tokenStorage
+    ) {
         parent::__construct($className);
 
         $this->postRepository = $postRepository;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function createByPostId($postId)
@@ -27,7 +35,13 @@ class CommentFactory extends Factory
         }
 
         $comment->setPost($post);
+        $comment->setAuthor($this->getUser());
 
         return $comment;
+    }
+
+    protected function getUser()
+    {
+        return  $this->tokenStorage->getToken()->getUser();
     }
 }
